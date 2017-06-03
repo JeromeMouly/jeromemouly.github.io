f7494b6b14df409b53395384faea7bd3995647b2
JeromeMouly <mouly.jerome@gmail.com>
2017-06-03 20:10:23 +0200

    Scroll Menu

--- js/init.js
+++ js/init.js
@@ -8,7 +8,7 @@
 
 // SrollTo
 $(document).ready(function() {
-    $('.js-scrollTo').on('click', function() { // Au clic sur un élément
+    $('a[href^="#"]').on('click', function() { // Au clic sur un élément
         var page = $(this).attr('href'); // Page cible
         var speed = 2000; // Durée de l'animation (en ms)
         $('html, body').animate({ scrollTop: $(page).offset().top }, speed); // Go
