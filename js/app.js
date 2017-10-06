ec321f04bf0cf9de7d9c2fa6aa8244b715f26cb8
JeromeMouly <mouly.jerome@gmail.com>
2017-10-06 08:52:10 +0200

    Creation Hero and Menu

---
+++ js/app.js
@@ -0,0 +1,9 @@
+/* ----- Menu ----- */
+
+function openMenu() {
+    document.getElementById("menu").style.height = "100%";
+}
+
+function closeMenu() {
+    document.getElementById("menu").style.height = "0%";
+}
