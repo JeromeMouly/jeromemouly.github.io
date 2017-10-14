8bb5bfd816330ed59d46823adf002b0b42286d46
JeromeMouly <mouly.jerome@gmail.com>
2017-10-14 13:23:10 +0200

    Second Commit

--- js/app.js
+++ js/app.js
@@ -6,3 +6,21 @@ function openMenu() {
 function closeMenu() {
     document.getElementById("menu").style.height = "0%";
 }
+
+
+/* ----- Portfolio Slider ----- */
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) { myIndex = 1 }
+    x[myIndex - 1].style.display = "block";
+    setTimeout(carousel, 4000); // Change image every 2 seconds
+}
